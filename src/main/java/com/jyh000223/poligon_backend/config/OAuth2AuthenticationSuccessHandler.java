4440cbf2e7f7d0b7dbde357db4d42b0452c3b41e
YH0223 <133633043+YH0223@users.noreply.github.com>
2025-11-14 19:30:14 +0900

    개발용 재수정

--- src/main/java/com/jyh000223/poligon_backend/config/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/jyh000223/poligon_backend/config/OAuth2AuthenticationSuccessHandler.java
@@ -84,7 +84,7 @@
                     .build();
 
             response.setHeader("Set-Cookie", cookie.toString());
-            response.sendRedirect("https://jyhdevstore.store/Frontend/");
+            response.sendRedirect("https://localhost:5173/Frontend/");
 
         }
 
